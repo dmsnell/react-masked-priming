d3dd45e4fda51b5247b8547f0f3a22839aff0879
Dennis Snell <dennis.snell@automattic.com>
2015-11-26 23:09:04 -0600

    Publish index files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 module.exports = {
 	entry: './src/app.jsx',
 	output: {
-		path: './build/',
+		path: './',
 		filename: 'index.js'
 	},
 	module: {
